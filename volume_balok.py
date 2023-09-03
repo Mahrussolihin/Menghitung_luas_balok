ef267ae492f23d3459b3139496ed60b9a5aad406
Mahrussolihin <143859887+Mahrussolihin@users.noreply.github.com>
2023-09-04 01:03:42 +0700

    Add files via upload
    Ini adalah coding sederhana untuk menghitung luas balok

---
+++ volume_balok.py
@@ -0,0 +1,39 @@
+print("Menghitung Volume Balok")
+print()
+
+print("Nama = Mahrus Solihin")
+print("NIM = 230441100032")
+print()
+
+#coding untuk mengetahui tipe datanya
+nama = "Mahrus Solihin"
+print(nama, "Tipenya", type(nama))
+NIM = 230441100032
+print(NIM, "Tipenya", type(NIM))
+print()
+
+print("Diketahui")
+print("Panjang balok = 24")
+print("Lebar balok = 18")
+print("Tinggi balok = 32")
+print()
+
+#coding untuk mengetahui tipe datanya
+panjang = 24
+print(panjang, "Tipenya", type(panjang))
+lebar = 18
+print(lebar, "Tipenya", type(lebar))
+tinggi = 32
+print(tinggi, "Tipenya", type(tinggi))
+print()
+
+print("Rumus volume balok adalah p*l*t")
+print("Maka p*l*t = 24*18*32")
+print()
+
+#rumus volume balok
+p = 24
+l = 18
+t = 32
+volume = p*l*t
+print("Jadi volune balok = ", format(volume))
